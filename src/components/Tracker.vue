d2c6be8411554857f25c2adf35ad491df1d7aa36
ANDRESROMEROH <andres.romero22@outlook.com>
2022-02-27 18:58:35 -0600

    Cleanup unused files

---
+++ src/components/Tracker.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+
+</script>
+
+<template>
+  <div>Place content here...</div>
+  <n-button type="primary">naive-ui</n-button>
+</template>
--- src/views/ExpenseTrackerView.vue
+++ src/views/ExpenseTrackerView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Tracker from '../components/Tracker.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Tracker />
   </main>
 </template>
